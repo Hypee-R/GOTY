217eb9d0b0b9bcdd85f814f50e60046877293390
Luis Altamirano <81448660+Hypee-R@users.noreply.github.com>
2021-08-23 10:04:21 -0500

    Remove credentials dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,7 @@ export const environment = {
   production: false,
   url: 'http://localhost:5000/login-app-udemy-a10ad/us-central1',
   firebase: {
-    apiKey: "AIzaSyAWptfIF3ZuFrfVCVxI-88L6Hodgh5aPe8",
-    authDomain: "login-app-udemy-a10ad.firebaseapp.com",
-    databaseURL: "https://login-app-udemy-a10ad-default-rtdb.firebaseio.com",
-    projectId: "login-app-udemy-a10ad",
-    storageBucket: "login-app-udemy-a10ad.appspot.com",
-    messagingSenderId: "805913671249",
-    appId: "1:805913671249:web:5110ebb8c7b59561076a70",
-    measurementId: "G-LGLRK638GT"
+    
   }
 };
 
